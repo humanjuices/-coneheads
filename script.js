b84992959cf6640a9da51c16b78aaeaa704ecc66
humanjuices <twinkletoadtoes@gmail.com>
2024-08-14 22:06:58 -0400

    adjusted random position generation

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const img_array = document.getElementsByClassName("image");
 
 for (var i = 0; i < img_array.length; i++) {
-  img_array[i].style.top = Math.floor(Math.random()*6000)+"px";
-  img_array[i].style.left = Math.floor(Math.random()*100)+"vw";
+  img_array[i].style.top = Math.floor(Math.random()*5500)+"px";
+  img_array[i].style.left = Math.floor(Math.random()*90)+"vw";
 }
 
 // document.getElementById("safety-first-button").addEventListener("click", loadAudio());
